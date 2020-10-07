de2cd2c25302cdc00f6f3e8c8928ba58ff1f4c96
ayce <raycel5lacap@gmail.com>
2020-10-06 19:51:30 -0700

    Change stage URL

--- client/src/js/game.js
+++ client/src/js/game.js
@@ -1,41 +1,41 @@
-// import GameView from "./gameView"
-// import * as THREE from "three"
+import GameView from "./gameView"
+import * as THREE from "three"
 
-// class Game {
-//  constructor() {
-//   this.createGameView();
-//  }
+class Game {
+ constructor() {
+  this.createGameView();
+ }
 
-//  createGameView() {
-//   //  scene size
-//   let width = window.innerWidth,
-//       height = window.innerHeight;
+ createGameView() {
+  //  scene size
+  let width = window.innerWidth,
+      height = window.innerHeight;
 
-//   let viewAngle = 75,
-//       aspect = width / height,
-//       near = 0.1,
-//       far = 10000;
+  let viewAngle = 75,
+      aspect = width / height,
+      near = 0.1,
+      far = 10000;
 
-//   let scene = new THREE.Scene();
-//   let camera = new THREE.PerspectiveCamera(
-//     viewAngle,
-//     aspect, 
-//     near,
-//     far
-//   );
+  let scene = new THREE.Scene();
+  let camera = new THREE.PerspectiveCamera(
+    viewAngle,
+    aspect, 
+    near,
+    far
+  );
 
-//   camera.position.z = 150;
+  camera.position.z = 150;
 
-//   let renderer = new THREE.WebGLRenderer();
-//   renderer.setSize(width, height);
-//   document.getElementById('game-canvas').appendChild(renderer.domElement);
+  let renderer = new THREE.WebGLRenderer();
+  renderer.setSize(width, height);
+  document.getElementById('game-canvas').appendChild(renderer.domElement);
 
-//    this.gameView = new GameView(
-//      renderer, camera, scene, this.key, this.musicDelay
-//    );
+   this.gameView = new GameView(
+     renderer, camera, scene, this.key, this.musicDelay
+   );
+   this.gameView.setup();
+ }
 
-//  }
+};
 
-// };
-
-// export default Game;
+export default Game;
